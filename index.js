8b5ef0c8267f077b21baf7a45a9585318bdbfdb1
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 10:24:21 +0100

    Añade la primera función de JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function main() {
     console.log("Starting...");
+    const titulo = document.querySelector("h1");
+    titulo.addEventListener("pointerenter", imprimeHola);
+}
+
+function imprimeHola(){
+    console.log("Hola");
 }
 
 window.addEventListener("load", main);
+
+//Definimos una función "e" y le indicamos que se llene con un selector que busque el valor del primer h1 encontrado.
+//const titulo = document.querySelector("h1");
+// Añadimos un evento a la constante "e" que haga que en la consola se indique "Hola" las veces que el raton pase por encima del <h1>
+//titulo.addEventListener("pointerenter", ()=>{console.log("Hola")})
