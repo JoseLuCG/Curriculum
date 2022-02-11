61b45ff413a7dc23d7f5fdf4585cfe49aa17e658
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 12:36:13 +0100

    Añadimos eventos a todo el body

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function main() {
-    console.log("Starting...");
-    const titulo = document.querySelector("h1");
-    titulo.addEventListener("pointerenter", imprimeHola);
-}
-
 function imprimeHola(){
     console.log("Hola");
 }
 
+
+function main() {
+    const elements = Array.from(document.body.querySelectorAll("*"));
+    console.log(elements)
+    console.log("Starting...");
+    const titulo = document.querySelector("h1");
+    titulo.addEventListener("pointerenter", imprimeHola);
+    for (let item of elements){
+        console.log(item)
+    }
+    for (let item of elements){
+        item.addEventListener("pointerenter", ()=>{console.log("Pop!")})
+    }
+}
 window.addEventListener("load", main);
 
 //Definimos una función "e" y le indicamos que se llene con un selector que busque el valor del primer h1 encontrado.
